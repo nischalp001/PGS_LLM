66e967dba5e902478a86b1902c98f2bf9cc7f8df
Nischal <2002nischal@gmail.com>
2025-06-18 13:37:15 +0545

    PDS documentation added and replaced by another. And, Prompt changed

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def ask_question(q: Query):
     context = "\n\n".join(top_chunks)
 
     prompt = (
-        f"You are an AI assistant answering based on this context:\n\n{context}\n\n"
+        f"You are an AI assistant talking as a human being tone answering based on this context and explain no more than 200 words:\n\n{context}\n\n"
         f"Question: {q.query}\nAnswer:"
     )
 
